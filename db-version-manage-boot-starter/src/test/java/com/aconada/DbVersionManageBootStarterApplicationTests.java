dea504c42b265a984f14345dd2f35a37510b9516
liujianqiang <liujianqiang@bjca.org.cn>
2021-12-14 18:12:01 +0800

    初始化提交

---
+++ db-version-manage-boot-starter/src/test/java/com/aconada/DbVersionManageBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aconada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbVersionManageBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
